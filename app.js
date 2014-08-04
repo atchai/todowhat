79fb64628fbc838f0d7f26c076f7ab5ca01fbe27
ackl <andrew.low@getdataseed.com>
2014-08-04 10:30:55 +0100

    add todo was not working

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ App.model = Backbone.Model.extend({
     defaults: {
         done: false
     },
-        localStorage: new Backbone.LocalStorage("StoredTodos"),
-
     toggle: function () {
         this.save({
             done: !this.get("done")
@@ -19,7 +17,7 @@ App.collection = Backbone.Collection.extend({
 })
 
 var item1, item2, item3, item4, collection, listofitems;
-var orderArray = [];
+
 
 App.view = Backbone.View.extend({
     initialize: function () {
@@ -48,7 +46,7 @@ App.view = Backbone.View.extend({
     render: function () {
         listofitems = '';
         collection.each(function (col) {
-            var tmpsting = '<li id=' + col.cid + '> <input type="checkbox">' + col.get('content') + '</li>';
+            var tmpsting = '<li id=' + col.get('id') + '> <input type="checkbox">' + col.get('content') + '</li>';
             listofitems = tmpsting.concat(listofitems);
         });
         var html = this.template({
@@ -60,36 +58,12 @@ App.view = Backbone.View.extend({
             collection.fetch();
             collection.each(function (col) {
                 if (col.get('done')) {
-                    var tmpid = '#' + col.cid;
+                    var tmpid = '#' + col.get('id');
                     console.log($(tmpid));
                     $(tmpid + ' input').attr('checked', 'checked');
                     $(tmpid).addClass('struck');
                 }
             });
-            var storedOrders = JSON.parse(localStorage["orders"]);
-            if (storedOrders) {
-            	listofitems = '';
-            	for (var i=0; i<storedOrders.length; i++) {
-        		var tmpcontent = collection.get({cid: storedOrders[i]}).get('content');
-        		console.log(tmpcontent);
-        		var tmpstring = '<li id="' + storedOrders[i] + '"> <input type="checkbox">' + tmpcontent + '</li>';
-        		console.log(tmpstring);
-        		listofitems += tmpstring;
-        		// var tmpstring = '<li id=' + storedOrders[i] + '> <input type="checkbox">'+collection.get({cid: storedOrders[i]}).get('content')+<'/li'>;
-        		// listofitems += tmpstring;
-        	}
-        	console.log(listofitems);
-        	$('ul').html(listofitems);
-        	collection.each(function (col) {
-                if (col.get('done')) {
-                    var tmpid = '#' + col.cid;
-                    console.log($(tmpid));
-                    $(tmpid + ' input').attr('checked', 'checked');
-                    $(tmpid).addClass('struck');
-                }
-            });
-            }
-			
             $('input[type="checkbox"]').click(function () {
                 if (this.checked) {
 
@@ -107,14 +81,12 @@ App.view = Backbone.View.extend({
         });
         $(document).ready(function () {
             $("#todoul").sortable({
-            	 update : function () {
-					var order = $('#todoul').sortable('toArray').toString();					
-            		console.log("Order: "+order);
-            		orderArray = $('#todoul').sortable('toArray');
-            		localStorage["orders"] = JSON.stringify(orderArray);
-	
+                 update : function () {
+                    var order = $('#todoul').sortable('toArray').toString();                    
+                    console.log("Order: "+order);
+            
 
-				}
+                }
             });
             $("#todoul").disableSelection();
         });
@@ -133,8 +105,8 @@ App.view = Backbone.View.extend({
                     content: todotmp
                 });
                 console.log('adding: ' + todotmp);
-                $('ul').prepend('<li>'+todotmp+'</li>');
-                	$('li:first').prepend('<input type="checkbox">');
+                // $('ul').prepend('<li>'+todotmp+'</li>');
+                //  $('li:first').prepend('<input type="checkbox">');
             }
             $('input[type="checkbox"]').click(function () {
                 if (this.checked) {
