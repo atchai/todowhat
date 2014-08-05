39f70ba7be4f091db8043035e186e20484fa0661
ackl <andrew.low@getdataseed.com>
2014-08-05 11:03:20 +0100

    added reordering persistance re: #7

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
 var App = App || {};
-var order = [];
-var rearrange = function(position) {
-    return collection.get({cid: value});
-}
+
 App.model = Backbone.Model.extend({
     defaults: {
-        done: false
+        done: false,
+        order: 0
     },
-    toggle: function () {
+    toggle: function() {
         this.save({
             done: !this.get("done")
         });
@@ -16,14 +14,13 @@ App.model = Backbone.Model.extend({
 
 App.collection = Backbone.Collection.extend({
     localStorage: new Backbone.LocalStorage("StoredTodos"),
-    model: App.model
+    model: App.model,
+    comparator: 'order'
 })
-
-var item1, item2, item3, item4, collection, listofitems;
 
 
 App.view = Backbone.View.extend({
-    initialize: function () {
+    initialize: function() {
         dummy1 = new App.model({
             content: 'review this ticket'
         });
@@ -46,39 +43,20 @@ App.view = Backbone.View.extend({
     },
     el: $("body"),
     template: _.template($('#test-template').html()),
-    render: function () {
+    render: function() {
         listofitems = '';
-        if (order.length == 0) {
-            collection.each(function (col) {
-                var tmpsting = '<li id=' + col.cid + '> <input type="checkbox">' + col.get('content') + '</li>';
-                listofitems = tmpsting.concat(listofitems);
-            });
-            var html = this.template({
-                test: '<h1>test</h1>',
-                todoslist: listofitems
-            });
-            this.$el.html(html);
-        }
-        else {
-            console.log('order array isnt empty');
-            $(document).ready(function(){
-                for (var i=0; i<order.length; i++)
-                {
-                    console.log(collection.get({cid: order[i]}).get('content'));
-                    var tmpstring = '<li id=' + order[i] + '> <input type="checkbox">' + collection.get({cid: order[i]}).get('content') + '</li>';
-                
-                listofitems += tmpstring;
-                }
-                console.log(listofitems);
-                $('#todoul').html(listofitems);
-                
-            });
-            
-
-        }
-        $(document).ready(function () {
+        collection.each(function(col) {
+            var tmpsting = '<li id=' + col.cid + '> <input type="checkbox">' + col.get('content') + '</li>';
+            listofitems = tmpsting.concat(listofitems);
+        });
+        var html = this.template({
+            test: '<h1>test</h1>',
+            todoslist: listofitems
+        });
+        this.$el.html(html);
+        $(document).ready(function() {
             collection.fetch();
-            collection.each(function (col) {
+            collection.each(function(col) {
                 if (col.get('done')) {
                     var tmpid = '#' + col.cid;
                     console.log($(tmpid));
@@ -86,31 +64,64 @@ App.view = Backbone.View.extend({
                     $(tmpid).addClass('struck');
                 }
             });
-            $('input[type="checkbox"]').click(function () {
+            $('input[type="checkbox"]').click(function() {
                 if (this.checked) {
 
                     // console.log($(this).closest('li').attr("id"))
                     // console.log(collection.get($(this).closest('li').attr("id")));
-                    collection.get($(this).closest('li').attr("id")).save({"done": true});
+                    collection.get($(this).closest('li').attr("id")).save({
+                        "done": true
+                    });
                     console.log(collection.get($(this).closest('li').attr("id")).get("done"));
                     $(this).closest('li').addClass('struck');
                 } else {
                     $(this).closest('li').removeClass('struck');
-                    collection.get($(this).closest('li').attr("id")).save({"done": false});
+                    collection.get($(this).closest('li').attr("id")).save({
+                        "done": false
+                    });
                     console.log(collection.get($(this).closest('li').attr("id")).get("done"));
                 }
             })
         });
-        $(document).ready(function () {
-
+        $(document).ready(function() {
             $("#todoul").sortable({
-                 update : function () {
-                    order = $('#todoul').sortable('toArray');                    
-                    console.log("Order: "+order);
+                update: function(event, ui) {
+                    var order = $('#todoul').sortable('toArray');
+                    console.log("Order: " + order);
+                    var cidOfDropped = ui.item.context.id;
+                    var itemIndex = ui.item.index();
+                    console.log('item index: ' + ui.item.index());
+                    console.log('item cid: ' + cidOfDropped);
+                    console.log('item below cid: ' + order[itemIndex + 1]);
+                    console.log('item below order' + collection.get({
+                        cid: order[itemIndex + 1]
+                    }).get('order'));
+                    collection.get({
+                        cid: cidOfDropped
+                    }).save({
+                        'order': collection.get({
+                            cid: order[itemIndex + 1]
+                        }).get('order') + 1
+                    });
+                    // console.log(collection.get({cid: cidOfDropped}).get('order'));
 
-                    localStorage["orderlist"] = JSON.stringify(order);
-            
-
+                    console.log(collection.get({
+                        cid: cidOfDropped
+                    }).get('content'));
+                    // collection.get({cid: cidOfDropped}).save({'order': }}           
+                    console.log(collection.get({
+                        cid: cidOfDropped
+                    }).get('order'));
+                    for (var i = 0; i < itemIndex; i++) {
+                        var currentOrder = collection.get({
+                            cid: order[i]
+                        }).get('order');
+                        collection.get({
+                            cid: order[i]
+                        }).save({
+                            "order": currentOrder + 2
+                        });
+                    }
                 }
             });
             $("#todoul").disableSelection();
@@ -119,27 +130,33 @@ App.view = Backbone.View.extend({
 
 
     },
-    addTodo: function (e) {
+    addTodo: function(e) {
         e.preventDefault();
-        $(document).ready(function () {
+        $(document).ready(function() {
             var todotmp = $('#todofield').val();
 
             if (todotmp) {
+                var todosAmount = collection.models.length;
+                var highestOrder;
+                if (todosAmount == 0) {
+                    console.log('no models in collection m8');
+                    collection.create({
+                        content: todotmp
+                    });
+                } else {
+                    highestOrder = collection.models[todosAmount - 1].get('order');
+                    collection.create({
+                        content: todotmp,
+                        order: highestOrder
+                    });
+                }
                 // collection.add({content: todotmp});
-                collection.create({
-                    content: todotmp
-                });
+
                 console.log('adding: ' + todotmp);
                 // $('ul').prepend('<li>'+todotmp+'</li>');
                 //  $('li:first').prepend('<input type="checkbox">');
-                cid = collection.at(collection.length - 1).cid;
-                console.log(cid);
-                if (order.length != 0){ //if list has been rearranged, order array wont be empty
-                    order.unshift(cid);
-                }
-
             }
-            $('input[type="checkbox"]').click(function () {
+            $('input[type="checkbox"]').click(function() {
                 if (this.checked) {
 
                     // console.log($(this).closest('li').attr("id"))
