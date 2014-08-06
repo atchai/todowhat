dc2c42be1df233cc14d77f60d73ca600e86db9f4
ackl <andrew.low@getdataseed.com>
2014-08-06 11:37:02 +0100

    removed redundant files

--- js/collections/todos-collection.js
+++
@@ -1,6 +0,0 @@
-app.TodosCollection = Backbone.Collection.extend({
-    localStorage: new Backbone.LocalStorage("StoredTodos"),
-    model: app.TodoModel,
-    comparator: 'order'
-})
-app.todos = new app.TodosCollection([]);
