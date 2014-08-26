46540a01c318a26526ee8286893cdb7230d6982a
andrew <andrew@lyra.(none)>
2014-08-26 17:50:00 +0100

    added gulp task for testing to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ var streamify = require('gulp-streamify')
 var browserify = require('browserify')
 var uglify = require('gulp-uglify')
 var gulp = require('gulp')
+var karma = require('karma').server;
 
+//default gulp task browserifies application js files in bundle and minifies js
 gulp.task('default', function() {
   var bundleStream = browserify('./js/app.js').bundle()
 
@@ -11,4 +13,18 @@ gulp.task('default', function() {
     .pipe(source('bundle.js'))
     .pipe(streamify(uglify()))
     .pipe(gulp.dest('./'))
+});
+
+//this task browserifies jasmine test specs
+gulp.task('browserify-test', function() {
+    return browserify('./tests/specs.js')
+        .bundle()
+        .pipe(source('testFile.js'))
+        .pipe(gulp.dest('./tests/'));
+});
+gulp.task('test', ['browserify-test'], function (done) {
+  karma.start({
+    configFile: __dirname + '/tests/my.conf.js',
+    singleRun: true
+  }, done);
 });
