d382d3167f73a5e1818ec225187b547b0a3b3344
ackl <andrewcklow@gmail.com>
2014-09-19 13:21:20 +0100

    missing semicolon

--- todowhat/static/tests/specs.js
+++ todowhat/static/tests/specs.js
@@ -1,7 +1,6 @@
 var Todo = require('../js/models/todo');
 
 describe("the todo", function(){
-
   beforeEach(function(){
     this.todo = new Todo({
       content: "A test todo"
@@ -11,26 +10,40 @@ describe("the todo", function(){
   it("should have content", function(){
     expect(this.todo.get("content")).toBe("A test todo");
   });
+
   it("should not be done yet", function(){
     expect(this.todo.get("done")).toBe(false);
   });
+
   it("should have no tags yet", function(){
     expect(this.todo.getTags()).not.toBeTruthy();
   });
 });
+
 describe("todo blank content", function(){
   beforeEach(function(){
     this.todo = new Todo({content: ''});
   });
+
   it("should not create the todo", function(){
     expect(this.todo.isValid()).not.toBeTruthy();
-  })
+  });
 });
 describe("todo content length greater than 255 chars", function(){
   beforeEach(function(){
     this.todo = new Todo({content: 'dafskljSadsfasdfafdsakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslsds'});
   });
+
   it("should not create the todo", function(){
     expect(this.todo.isValid()).not.toBeTruthy();
-  })
+  });
+});
+describe("todo with tags", function(){
+  beforeEach(function(){
+    this.todo = new Todo({content: 'A test todo with tags', tags: ['tag1','tag2','tag3']});
+  });
+
+  it("should have three tags", function(){
+    expect(this.todo.getTags().length).toEqual(3);
+  });
 });
--- todowhat/static/tests/testFile.js
+++ todowhat/static/tests/testFile.js
@@ -13312,37 +13312,61 @@ module.exports = Backbone.Model.extend({
 var Todo = require('../js/models/todo');
 
 describe("the todo", function(){
- 
   beforeEach(function(){
     this.todo = new Todo({
       content: "A test todo"
     });
   });
- 
+
   it("should have content", function(){
     expect(this.todo.get("content")).toBe("A test todo");
   });
+
   it("should not be done yet", function(){
     expect(this.todo.get("done")).toBe(false);
   });
+
   it("should have no tags yet", function(){
     expect(this.todo.getTags()).not.toBeTruthy();
-  }); 
+  });
 });
+
 describe("todo blank content", function(){
   beforeEach(function(){
     this.todo = new Todo({content: ''});
   });
+
   it("should not create the todo", function(){
     expect(this.todo.isValid()).not.toBeTruthy();
-  })
+  });
 });
 describe("todo content length greater than 255 chars", function(){
   beforeEach(function(){
     this.todo = new Todo({content: 'dafskljSadsfasdfafdsakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslkfjsakldjfkaldsjfakdafskljakfj;aslsds'});
   });
+
   it("should not create the todo", function(){
     expect(this.todo.isValid()).not.toBeTruthy();
-  })
+  });
 });
+describe("todo with tags", function(){
+  beforeEach(function(){
+    this.todo = new Todo({content: 'A test todo with tags', tags: ['tag1','tag2','tag3']});
+  });
+
+  it("should have three tags", function(){
+    expect(this.todo.getTags().length).toEqual(3);
+  });
+});
+describe("todo with duplicate tags", function() {
+  beforeEach(function() {
+    this.todo = new Todo({
+      content: 'A test todo with duplicate tags',
+      tags: ['tag1','tag1','tag2','tag1','tag3','tag2']
+    });
+  });
+  it("should again have three tags", function() {
+    expect(this.todo.getTags().length).toEqual(3);
+  })
+})
 },{"../js/models/todo":"/home/andrew/dev/flask-what-todo/todowhat/static/js/models/todo.js"}]},{},["/home/andrew/dev/flask-what-todo/todowhat/static/tests/specs.js"])
