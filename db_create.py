278670adc8d5a3e510b6d39880f2dab948b064f3
ackl <andrewcklow@gmail.com>
2014-09-19 16:59:33 +0100

    fix db_create script

--- db_create.py
+++ db_create.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 from migrate.versioning import api
-from config import SQLALCHEMY_DATABASE_URI
-from config import SQLALCHEMY_MIGRATE_REPO
+from config.DevConfiguration import SQLALCHEMY_DATABASE_URI
+from config.DevConfiguration import SQLALCHEMY_MIGRATE_REPO
 import os.path
 
 from todowhat import db
