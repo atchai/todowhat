cff2fcbdac78490718e5101a2907b84a8f793e16
ackl <andrewcklow@gmail.com>
2014-09-19 13:20:07 +0100

    refactor tests into relevant classes

--- tests.py
+++ tests.py
@@ -7,9 +7,12 @@ from flask import json
 import unittest
 
 
-class TodoWhatTests(TestCase):
+class BaseTestCase(TestCase):
+    """
+        Base class with app setup and helper functions for other
+        test classes to inherit from.
+    """
 
-    # Setup
     def setUp(self):
         """Set up test database"""
         db.create_all()
@@ -35,6 +38,10 @@ class TodoWhatTests(TestCase):
     def logout(self):
         return self.client.get('/logout')
 
+    def login_dummy_user(self):
+        self.make_dummy_user()
+        self.login('admin@admin', 'admin')
+
     def make_dummy_user(self):
         """Creates a dummy user and adds it to the session"""
         pwdhash = generate_password_hash('admin')
@@ -53,14 +60,32 @@ class TodoWhatTests(TestCase):
         data = json.dumps(dict(content='a unittest todo'))
         return self.client.post('/todos', data=data,
                                 content_type='application/json')
-    # Test methods
 
-    # Authentication
+
+class UnauthenticatedTests(BaseTestCase):
 
     def test_unauthenticated(self):
         """Ensure no user logged in by default"""
         response = self.client.get("/auth")
         self.assertEquals(response.json, dict(authenticated=False))
+
+    def test_unauthenticated_get_todos(self):
+        """Ensure attempt to get todos requires login"""
+        response = self.client.get("/todos")
+        self.assertRedirects(response, '/login/?next=%2Ftodos')
+
+    def test_unauthenticated_get_tags(self):
+        """Ensure attempt to get tags requires login"""
+        response = self.client.get("/tags")
+        self.assertRedirects(response, '/login/?next=%2Ftags')
+
+    def test_unauthenticated_post_todos(self):
+        """Ensure todos can not be created on server without login"""
+        response = self.make_todo()
+        self.assertRedirects(response, '/login/?next=%2Ftodos')
+
+
+class AuthenticationTests(BaseTestCase):
 
     def test_users_can_login(self):
         """Ensure users can login to the application"""
@@ -87,46 +112,39 @@ class TodoWhatTests(TestCase):
             self.client.get(user.get_activation_link())
             self.assertEquals(user.activated, True)
 
-    def test_unauthenticated_get_todos(self):
-        """Ensure attempt to get todos requires login"""
-        response = self.client.get("/todos")
-        self.assertRedirects(response, '/login/?next=%2Ftodos')
-
-    def test_unauthenticated_get_tags(self):
-        """Ensure attempt to get tags requires login"""
-        response = self.client.get("/tags")
-        self.assertRedirects(response, '/login/?next=%2Ftags')
-
-    def test_unauthenticated_post_todos(self):
-        """Ensure todos can not be created on server without login"""
-        response = self.make_todo()
-        self.assertRedirects(response, '/login/?next=%2Ftodos')
-
-    # Authorization
-
-    def test_authorized_get_todos(self):
-        """Ensure a logged in user can GET todos"""
+    # Error Testing
+    def test_404_error(self):
+        """Ensure 404 raised on unexisting resource"""
         with self.client:
-            self.make_dummy_user()
-            self.login('admin@admin', 'admin')
-            response = self.client.get("/todos")
-            self.assert_200(response, message=None)
+            response = self.client.get('/thisresourcedoesntexit')
+            self.assert_404(response, message=None)
+
+    def test_405_error(self):
+        """Ensure 405 raised for forbidden methods"""
+        with self.client:
+            response = self.client.post('/tags',
+                                        data="tag",
+                                        content_type='application/json')
+            self.assert_405(response, message=None)
+
+
+class UnauthorizedTests(BaseTestCase):
+
+    def make_todo_login_second_user(self):
+        self.make_dummy_user()
+        self.make_second_dummy_user()
+        # Log the first user in and create a todo.
+        self.login('admin@admin', 'admin')
+        self.make_todo()
+        # Log the first user out.
+        self.logout()
+        # Log the second user in, send GET to /todos
+        self.login('admin2@admin', 'admin')
 
     def test_unauthorized_get_todos(self):
         """Ensure a user cannot get the todos of another user"""
         with self.client:
-            # Create two dummy users.
-            self.make_dummy_user()
-            self.make_second_dummy_user()
-
-            # Log the first user in and create a todo.
-            self.login('admin@admin', 'admin')
-            self.make_todo()
-            # Log the first user out.
-            self.logout()
-
-            # Log the second user in, send GET to /todos
-            self.login('admin2@admin', 'admin')
+            self.make_todo_login_second_user()
             response = self.client.get('/todos')
             # Response should return empty array of todos
             self.assertEquals(response.json, dict(todos=[]))
@@ -134,11 +152,45 @@ class TodoWhatTests(TestCase):
             response = self.client.get('/todos/1')
             self.assert_401(response, message=None)
 
+    def test_unauthorized_put_todo(self):
+        """Ensure a user cannot change the todos of another user"""
+        with self.client:
+            self.make_todo_login_second_user()
+            data = json.dumps(dict(order=2))
+            response = self.client.put('/todos/1', data=data,
+                                       content_type='application/json')
+            self.assert_401(response, message=None)
+
+    def test_unauthorized_delete_todo(self):
+        """Ensure a user cannot delete the todos of another user"""
+        with self.client:
+            self.make_todo_login_second_user()
+            response = self.client.delete('/todos/1')
+            self.assert_401(response, message=None)
+
+
+class AuthorizationTests(BaseTestCase):
+
+    def test_authorized_get_todos(self):
+        """Ensure a logged in user can GET todos"""
+        with self.client:
+            self.login_dummy_user()
+            response = self.client.get("/todos")
+            self.assert_200(response, message=None)
+
+    def test_authorized_post_todos(self):
+        """Ensure a logged in user can create a todo on the server"""
+        with self.client:
+            # Create a dummy user and add it to the session
+            self.login_dummy_user()
+
+            response = self.make_todo()
+            self.assert_status(response, 201, message=None)
+
     def test_authorized_put_todos(self):
         """Ensure a logged in user can PUT todos to update them"""
         with self.client:
-            self.make_dummy_user()
-            self.login('admin@admin', 'admin')
+            self.login_dummy_user()
             # Create dummy data and make it json
             self.make_todo()
 
@@ -159,50 +211,28 @@ class TodoWhatTests(TestCase):
             response = self.client.get('/todos/1')
             self.assertEquals(response.json['tags'], tags_data['tags'])
 
-    def test_authorized_post_todos(self):
-        """Ensure a logged in user can create a todo on the server"""
-        with self.client:
-            # Create a dummy user and add it to the session
-            self.make_dummy_user()
-            # Login with the dummy user
-            self.login('admin@admin', 'admin')
-
-            response = self.make_todo()
-            self.assert_status(response, 201, message=None)
-
-    def test_post_todos_invalid_JSON(self):
-        """Ensure a todo cannot be created if invalid JSON is posted"""
-        with self.client:
-            # Create a dummy user and add it to the session
-            self.make_dummy_user()
-            # Login with the dummy user
-            self.login('admin@admin', 'admin')
-
-            # Try to post the data to server
-            response = self.client.post('/todos',
-                                        data="This is totally valid JSON mate",
-                                        content_type='application/json')
-            self.assert_status(response, 400, message=None)
-
     def test_authorized_delete_todo(self):
         """Ensure a logged in user can delete a todo"""
         with self.client:
-            self.make_dummy_user()
-            # Login with the dummy user
-            self.login('admin@admin', 'admin')
-
-            #  # Create dummy data and make it json
-            # data = json.dumps(dict(content='a unittest for delete todo'))
-
-            # # Try to post the data to server
-            # self.client.post('/todos', data=data,
-            #                  content_type='application/json')
+            self.login_dummy_user()
             self.make_todo()
 
             response = self.client.delete('/todos/1')
             self.assert_200(response, message=None)
             response = self.client.get('/todos')
             self.assertEquals(response.json, dict(todos=[]))
+
+    def test_post_todos_invalid_JSON(self):
+        """Ensure a todo cannot be created if invalid JSON is posted"""
+        with self.client:
+            # Create a dummy user and add it to the session
+            self.login_dummy_user()
+            # Try to post the data to server
+            response = self.client.post('/todos',
+                                        data="This is totally valid JSON mate",
+                                        content_type='application/json')
+            self.assert_status(response, 400, message=None)
+
 
 if __name__ == '__main__':
     unittest.main()
